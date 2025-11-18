7fdc19f737f3e6dfe0bce79942cef2a7a58737f1
Anson Chung <ansonchung3@gmail.com>
2025-11-18 15:40:55 +0000

    Add GET endpoint for filtered data, with stand alone dummy data and docs

--- server.ts
+++ server.ts
@@ -1,9 +1,25 @@
 import express from 'express';
 const app = express();
-import animalsData from './DummyData.json' with { type: "json" }; // static import with assertion
-
+import animalsData from './DummyData.json' with { type: 'json' }; // static import with assertion
+import data from './DUMMY.json' with { type: 'json' }; // static import with assertion
 
 // type animals: AnimalRecord = animalsData;
+
+app.get('/details/:state', (req, res) => {
+    console.log('end point');
+    const state = req.params.state;
+    const resData = data.data;
+    if (!state || !checkStateParam(state)) {
+        res.send(resData);
+    } else {
+        console.log(`state is good, it's ${state}`);
+        res.send(resData.filter((animal) => animal.state === state));
+    }
+});
+
+function checkStateParam(param: string) {
+    return param === 'rescued' || param === 'adopted';
+}
 
 app.get('/', (req, res) => {
     res.send(animalsData);
